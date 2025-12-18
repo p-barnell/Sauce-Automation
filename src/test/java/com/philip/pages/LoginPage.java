5b9b57a5f982c131f1b612853fbe8f7d004177d3
Philip Barnell <pbarnell@gmail.com>
2025-12-18 20:49:00 +0000

    Implemented Explicit Waits in Loginpage

--- src/test/java/com/philip/pages/LoginPage.java
+++ src/test/java/com/philip/pages/LoginPage.java
@@ -2,9 +2,13 @@ package com.philip.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import java.time.Duration;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage {
   private WebDriver driver;
+  private WebDriverWait wait;
   
   // 1. LOCATORS (The "map" of the page)
   // We keep these private so the Test doesn't touch them directly
@@ -17,19 +21,27 @@ public class LoginPage {
   // The test passes the 'driver' to this class so it can control the browser.
   public LoginPage(WebDriver driver) {
 	  this.driver = driver;
+	  // Initialise the wait (Give it a 10-second timeout)
+	  this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
   }
   
   // 3. ACTIONS (The "Buttons" on the remote control)
   
   public void enterUsername(String username) {
+	  //wait until the box is visible, then type
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField));	  
 	  driver.findElement(usernameField).sendKeys(username);
   }
   
   public void enterPassword(String password) {
+	  //wait until the box is visible, then type
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(passwordField));
 	  driver.findElement(passwordField).sendKeys(password);
   }
   
   public void clickLogin() {
+	  //wait until the button is visible, then click
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(loginButton));
 	  driver.findElement(loginButton).click();
   }
   
@@ -42,6 +54,8 @@ public class LoginPage {
   
   // For negative testing later
   public String getErrorMessage() {
+	  //wait until the error message is on the screen
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage));
 	  return driver.findElement(errorMessage).getText();
   }
   
